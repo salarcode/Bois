6a31da07e2654b2fb1424168773bdcaad18e4d4c
salar2k <salar2k@gmail.com>
2014-04-29 23:06:36 +0430

    DateTime test is added.

---
+++ Salar.Bois.Tests/Objects/BasicTypesDateTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+namespace Salar.Bois.Tests.Objects
+{
+	public class BasicTypesDateTime : IBaseType
+	{
+		public string Name { get; set; }
+		public DateTime? BirthDate { get; set; }
+		public DateTime RegDate { get; set; }
+		public int Age { get; set; }
+		public int Children { get; set; }
+
+		public void Initialize()
+		{
+			Name = "Salar Khalilzadeh";
+			BirthDate = new DateTime(2014, 2, 25, 4, 0, 16);
+			RegDate = new DateTime(2014, 2, 25, 4, 0, 16, DateTimeKind.Utc);
+			Age = 27;
+			Children = 10;
+		}
+	}
+}

d0a68aae9594f804ad7ba6dab1853a4e9616665b
salarcode <salar2k@gmail.com>
2018-09-09 10:54:20 +1000

    Testing complex struct type

---
+++ Salar.Bois.NetFx.Tests/TestObjects/TestStructComplex.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salar.Bois.NetFx.Tests.TestObjects
+{
+	public struct TestStructComplex
+	{
+		public int ID { get; set; }
+
+		public Size? SizeNullF;
+
+		public Collection<TestStructComplex> Collection { get; set; }
+
+		public List<TestStructComplex> List { get; set; }
+
+		public Dictionary<Size,TestStructComplex> Dictionary { get; set; }
+
+		public static IEnumerable<object[]> GetTestData()
+		{
+			yield return new object[]
+			{
+				new TestStructComplex
+				{
+					ID = short.MaxValue,
+					List = new List<TestStructComplex>()
+					{
+						new TestStructComplex()
+						{
+							ID = byte.MaxValue,
+							Collection = new Collection<TestStructComplex>()
+							{
+								new TestStructComplex()
+								{
+									ID = ushort.MaxValue,
+									SizeNullF = new Size(90, 10)
+								}
+							},
+							Dictionary = new Dictionary<Size, TestStructComplex>()
+							{
+								{
+									new Size(10, 10), new TestStructComplex()
+									{
+										ID = 90
+									}
+								}
+							}
+						}
+					},
+					SizeNullF = new Size(10, 222)
+				}
+			};
+		}
+	}
+
+}
--- Salar.Bois.NetFx.Tests/Tests/Test_Complex_Struct.cs
+++ Salar.Bois.NetFx.Tests/Tests/Test_Complex_Struct.cs
@@ -11,6 +11,22 @@ namespace Salar.Bois.NetFx.Tests.Tests
 	public class Test_Complex_Struct : TestBase
 	{
 		[Theory]
+		[MemberData(nameof(TestStructComplex.GetTestData), MemberType = typeof(TestStructComplex))]
+		public void TestingStructComplex(TestStructComplex init)
+		{
+			ResetBois();
+
+			BoisSerializer.Initialize<TestStructComplex>();
+
+			Bois.Serialize(init, TestStream);
+			ResetStream();
+
+			var final = Bois.Deserialize<TestStructComplex>(TestStream);
+
+			SerializeAreEqual(init, final);
+		}
+
+		[Theory]
 		[MemberData(nameof(TestStructPrimitives.GetTestData), MemberType = typeof(TestStructPrimitives))]
 		public void TestingStructSelfReferencing(TestStructPrimitives init)
 		{

1cd47a109d908b321756b9bd95235d029b606047
salarcode <salar2k@gmail.com>
2018-09-09 16:53:13 +1000

    Version bump to 3.0.0

--- Salar.Bois.NetFx/Properties/AssemblyInfo.cs
+++ Salar.Bois.NetFx/Properties/AssemblyInfo.cs
@@ -5,37 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Salar.Bois.EmitNet4")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Salar.Bois.EmitNet4")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTitle("Salar.Bois")]
+[assembly: AssemblyDescription("Salar's Serializer for Binary Object Indexed Serialization")]
+[assembly: AssemblyCompany("Salar Khalilzadeh")]
+[assembly: AssemblyProduct("Salar's Binary Object Indexed Serialization")]
+[assembly: AssemblyCopyright("MPLv2 © Salar Khalilzadeh 2018")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f247f38a-8bfa-4b7a-ab4e-12c646402f9c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-//#if DEBUG
-[assembly: InternalsVisibleTo("Salar.Bois.EmitPlayground")]
-//#endif
-[assembly: InternalsVisibleTo("Salar.Bois.NetFx.Tests")]
-[assembly: InternalsVisibleTo("Salar.Bois.PlayBenchmark")]
+[assembly: InternalsVisibleTo("Salar.Bois.NetFx.Tests,PublicKey=00240000048000009400000006020000002400005253413100040000010001008d0208f4a63105792422c5b67cd6cf537d9a56f4d82e060dcae4dd9a01e6c5c7946dfb6c7dbfe7d021486ac6e955ea4719b74d78f7d625d5d602ffcdb85bca08175d982e3cdecbbadc833e599b8e8338f08abf47deef8b1e0dd67917dcf39345659d546e42e11c3a1157eb3e27dfd8de2cb33baf7872d0bf106c79c668e340a5")]
+[assembly: InternalsVisibleTo("Salar.Bois.EmitPlayground,PublicKey=00240000048000009400000006020000002400005253413100040000010001008d0208f4a63105792422c5b67cd6cf537d9a56f4d82e060dcae4dd9a01e6c5c7946dfb6c7dbfe7d021486ac6e955ea4719b74d78f7d625d5d602ffcdb85bca08175d982e3cdecbbadc833e599b8e8338f08abf47deef8b1e0dd67917dcf39345659d546e42e11c3a1157eb3e27dfd8de2cb33baf7872d0bf106c79c668e340a5")]

97293f4a51eed37be10b91ba1388a584794d3f8a
salarcode <salar2k@gmail.com>
2019-07-30 19:59:15 +1000

    BoisLz4Serializer code updated

--- Salar.Bois.LZ4/BoisLz4Serializer.cs
+++ Salar.Bois.LZ4/BoisLz4Serializer.cs
@@ -37,101 +37,51 @@ namespace Salar.Bois.LZ4
 		{
 			using (var mem = new MemoryStream())
 			{
-				_serializer.Serialize(obj, mem/*, TODO: store compression flag*/);
+				_serializer.Serialize(obj, mem);
 
 				var serializedBuff = mem.GetBuffer();
 				var length = (int)mem.Length;
 
 				var compressedBuff = LZ4Pickler.Pickle(serializedBuff, 0, length, lz4Level);
-				output.Write(compressedBuff, 0, length);
-
-				//var outputBuff = new byte[LZ4Codec.MaximumOutputSize(length)];
-				//var finalLength = LZ4Codec.Encode(serializedBuff, 0, length, outputBuff, 0, outputBuff.Length, lz4Level);
-
-				//if (finalLength + 1 > length)
-				//{
-				//	// TODO: update the very first index byte and flag compressed as true
-				//	WriteCompressedFlag(compressed: true, output);
-				//	// compressed size is smaller
-				//	output.Write(outputBuff, 0, finalLength);
-				//}
-				//else
-				//{
-				//	WriteCompressedFlag(compressed: true, output);
-
-				//	// compressed size is not small enough
-				//	output.Write(serializedBuff, 0, length);
-				//}
+				output.Write(compressedBuff, 0, compressedBuff.Length);
 			}
 		}
 
 		public T Deserialize<T>(Stream objectData)
 		{
-			using (var mem = new MemoryStream())
+			byte[] compressedBuff = null;
+			MemoryStream mem;
+			using (mem = new MemoryStream())
 			{
-				objectData.CopyTo(mem);
+				if (objectData is MemoryStream outMem)
+					try
+					{
+						compressedBuff = outMem.GetBuffer();
+					}
+					catch (UnauthorizedAccessException)
+					{
+						// eat the error
+					}
 
-				var compressedBuff = mem.GetBuffer();
-				var length = (int)mem.Length;
+				int length;
+				if (compressedBuff == null)
+				{
+					objectData.CopyTo(mem);
+					compressedBuff = mem.GetBuffer();
+					length = (int)mem.Length;
+				}
+				else
+				{
+					length = compressedBuff.Length;
+				}
 
 				var serializedBuff = LZ4Pickler.Unpickle(compressedBuff, 0, length);
 
-				mem.Position = 0;
-				mem.SetLength(serializedBuff.Length);
-				mem.Write(serializedBuff, 0, serializedBuff.Length);
-				mem.Position = 0;
+				mem.Dispose();
+				mem = new MemoryStream(serializedBuff);
 
 				return _serializer.Deserialize<T>(mem);
 			}
 		}
-		public T Deserialize_Temp<T>(Stream objectData)
-		{
-			// TODO: read the very first index byte and check the flag if the buffer is compressed
-
-			var isCompressed = ReadCompressedFlag(objectData);
-			if (isCompressed)
-			{
-				using (var mem = new MemoryStream())
-				{
-					objectData.CopyTo(mem);
-
-					var compressedBuff = mem.GetBuffer();
-					var length = (int)mem.Length;
-
-
-
-					//LZ4Codec.Decode(compressedBuff, 0, compressedBuff.Length);
-				}
-			}
-			else
-			{
-				return _serializer.Deserialize<T>(objectData);
-			}
-			throw new NotImplementedException();
-		}
-
-		/// <summary>
-		/// 0010 0000 = 32 -- data is compressed
-		/// </summary>
-		internal const byte FlagCompressed = 0b0_0_1_0_0_0_0_0;
-
-		/// <summary>
-		/// 0000 0000 = 0 -- data is not compressed
-		/// </summary>
-		internal const byte FlagUncompressed = 0b0_0_0_0_0_0_0_0;
-
-		private void WriteCompressedFlag(bool compressed, Stream stream)
-		{
-			if (compressed)
-				stream.WriteByte(FlagCompressed);
-			else
-				stream.WriteByte(FlagUncompressed);
-		}
-
-		private bool ReadCompressedFlag(Stream stream)
-		{
-			return stream.ReadByte() == FlagCompressed;
-		}
-
 	}
 }

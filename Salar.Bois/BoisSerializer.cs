d4331f4421fb8fec3239819703e8acc973dceb8b
Darek Danielewski <dydek@hotmail.com>
2019-12-26 12:15:30 -0600

    We should allow non-generic Serialize as well

--- Salar.Bois/BoisSerializer.cs
+++ Salar.Bois/BoisSerializer.cs
@@ -104,7 +104,7 @@ namespace Salar.Bois
 		/// <param name="obj">The object to be serialized.</param>
 		/// <param name="type">The object type.</param>
 		/// <param name="output">The output of the serialization in binary.</param>
-		internal void Serialize(object obj, Type type, Stream output)
+		public void Serialize(object obj, Type type, Stream output)
 		{
 			if (obj == null)
 				throw new ArgumentNullException(nameof(obj), "Object cannot be null.");
